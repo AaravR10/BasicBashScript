3867dca6c592731efd587b0322a3b81cd87b6adc
Aarav Ragoonanan <127631010+AaravR10@users.noreply.github.com>
2024-11-08 10:43:15 -0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
-
+#If you're wondering why there's so many comments, no I don't actually need this many comments
+#It's if anyone else needs the comments
+#Checks for root access, if no root access, script will rerun itself with root access
 if [ "$EUID" -ne 0 ]; then
   sudo "$0" "$@"
   exit
 fi
 
+#MENU
 echo "Cyberpatriot Basic Script"
 echo "Firewall......................1"
 echo "Updates and Upgrades..........2"
 echo "Configure /etc/login.defs.....3"
 
+#Asks for the command to run
 read -p "Which command do you want to run? " command_value
 
+#Firewall enable and possible install if not already installed
 if [[ $command_value == 1 ]]; then
   if ! dpkg -l | grep -q "ufw"; then
     sudo apt-get install ufw
@@ -22,11 +27,13 @@ if [[ $command_value == 1 ]]; then
   echo "Firewall is enabled"
 fi
 
+#Get updates and upgrades
 if [[ $command_value == 2 ]]; then
   sudo apt-get update && sudo apt-get upgrade -y
   echo "apt-update and apt-upgrade run"
 fi
 
+#COnfigure /etc/login.defs
 if [[ $command_value == 3 ]]; then
   sed -i 's/^PASS_MAX_DAYS.*$/PASS_MAX_DAYS  15/' /etc/login.defs
     echo "Password maximum age set"
